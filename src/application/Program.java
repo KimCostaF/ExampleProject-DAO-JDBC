8087846ee19251f6bdc0983cc5fc97f1fa559b56
Kim Costa Ferro Alves <kcostaferro@gmail.com>
2022-12-07 16:39:43 -0300

    Classe Entidade "Seller"

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department dp = new Department(3,"Tvs");
         System.out.println(dp);
+
+        Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),5500.60,dp);
+        System.out.println(seller);
+
     }
 }
