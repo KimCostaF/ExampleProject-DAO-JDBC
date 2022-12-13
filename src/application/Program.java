a5ab67613e8df720dad030fd98bc0f02e5468b02
Kim Costa Ferro Alves <kcostaferro@gmail.com>
2022-12-13 10:58:36 -0300

    FindById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.DAO.DaoFactory;
+import model.DAO.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -7,10 +9,19 @@ import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
+
+        /*
         Department dp = new Department(3,"Tvs");
         System.out.println(dp);
 
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),5500.60,dp);
+        System.out.println(seller);
+
+         */
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        Seller seller = sellerDao.findById(3);
+
         System.out.println(seller);
 
     }
