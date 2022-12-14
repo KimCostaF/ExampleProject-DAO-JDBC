279b3ddbf79413db5a570ebcfdc2b854e20f3050
Kim Costa Ferro Alves <kcostaferro@gmail.com>
2022-12-14 10:26:58 -0300

    Reusing instantiation

--- src/model/impl/SellerDaoJDBC.java
+++ src/model/impl/SellerDaoJDBC.java
@@ -47,18 +47,8 @@ public class SellerDaoJDBC implements SellerDao {
             st.setInt(1,id);
             rs = st.executeQuery();
             if (rs.next()){
-                Department dep = new Department();
-                dep.setId(rs.getInt("DepartmentId"));
-                dep.setName(rs.getString("DepName"));
-
-                Seller obj = new Seller();
-                obj.setId(rs.getInt("Id"));
-                obj.setName(rs.getString("Name"));
-                obj.setEmail(rs.getString("Email"));
-                obj.setSalary(rs.getDouble("BaseSalary"));
-                obj.setBirthdate(rs.getDate("BirthDate"));
-                obj.setDp(dep);
-
+                Department dep = instatiateDepartment(rs);
+                Seller obj = instatiateSeller(rs,dep);
                 return obj;
             }
         } catch (SQLException e) {
@@ -72,6 +62,26 @@ public class SellerDaoJDBC implements SellerDao {
 
 
         return null;
+    }
+
+    private Seller instatiateSeller(ResultSet rs, Department dep) throws SQLException {
+       Seller obj = new Seller();
+        obj.setId(rs.getInt("Id"));
+        obj.setName(rs.getString("Name"));
+        obj.setEmail(rs.getString("Email"));
+        obj.setSalary(rs.getDouble("BaseSalary"));
+        obj.setBirthdate(rs.getDate("BirthDate"));
+        obj.setDp(dep);
+
+        return obj;
+    }
+
+     private Department instatiateDepartment(ResultSet rs) throws SQLException {
+        Department dep = new Department();
+        dep.setId(rs.getInt("DepartmentId"));
+        dep.setName(rs.getString("DepName"));
+
+        return dep;
     }
 
     @Override
